7316a25e98ced2f4c445240138362c47e33b7078
Solar-Chutney <noam4134@gmail.com>
2021-10-08 14:58:39 +0100

    update loading bmp

--- src/bmp.h
+++ src/bmp.h
@@ -10,33 +10,122 @@
 #define BMP_FILE_HEADER_BYTES 14
 #define BMP_INFO_HEADER_BYTES 40
 
-int bmpError(const char* path)
+
+int checkValidBMPFile(size_t file_size, char *file_data)
 {
-  printf("file at %s is not a valid BMP file\n", path);
-  return IMG_FAILURE;
+   //check file is big enough 
+  if(file_size < BMP_FILE_HEADER_BYTES + BMP_INFO_HEADER_BYTES)
+  {
+    printf("file is an inappropriate size\n");
+    return 0;
+  }
+  //check file has a bmp ID field
+  if(file_data[0] != 'B' || file_data[1] != 'M')
+  {
+    printf("file does not have a BMP file signature\n");
+    return 0;
+  }
+  //check internal file specified size matches actual file size
+  size_t interal_bmp_file_size = (size_t)(file_data[2] | file_data[3] << 8 | file_data[4] << 16 | file_data[5] << 24);
+  if(interal_bmp_file_size != 0)
+  {
+    printf("error, file might be corrupt, the file's size was %d, but the file specified a size of %d", file_size, interal_bmp_file_size);
+    return 0;
+  }
+  //check if file specified header size matches FILEHEADER + BITMAPINFOHEADER size
+  size_t internal_bmp_header_size = (size_t)(file_data[10] | file_data[11] << 8 | file_data[12] << 16 | file_data[13] << 24);
+  if(internal_bmp_header_size != BMP_FILE_HEADER_BYTES + BMP_INFO_HEADER_BYTES)
+  {
+    printf("error, loader does not support a header of size %d, only BITMAPINFOHEADER is supported\n");
+    return 0;
+  }
+
+  return 1;
+}
+
+int fillImageInfoFromFile(img *img, char *file_data)
+{
+  //header offset
+  size_t ho = BMP_FILE_HEADER_BYTES;
+  char info_header[BMP_INFO_HEADER_BYTES];
+
+  size_t interal_bmp_header_size =
+     (size_t)(file_data[ho + 0] | file_data[ho + 1] << 8 | file_data[ho + 2] << 16 | file_data[ho + 3] << 24);   
+  if(interal_bmp_header_size != BMP_INFO_HEADER_BYTES) 
+  {
+    printf("header is not BITMAPINFOHEADER, only BITMAPINFOHEADER is supported!\n");
+    return 0;
+  }
+
+  img->width = (uint)(file_data[ho + 4] | file_data[ho + 5] << 8 | file_data[ho + 6] << 16 | file_data[ho + 7] << 24);   
+
+  img->height = (uint)(file_data[ho + 8] | file_data[ho + 9] << 8 | file_data[ho + 10] << 16 | file_data[ho + 11] << 24); 
+	//no. of planes 12-13
+	//bits per pixel
+	size_t bits_per_pixel = (uint)(file_data[ho + 14] | file_data[ho + 15] << 8);
+  if(bits_per_pixel == 32)
+  {
+    img->bytes_per_channel = 1;
+    img->channel_count = 4;
+  }
+  else if(bits_per_pixel == 24)
+  {
+    img->bytes_per_channel = 1;
+    img->channel_count = 3;
+  }
+  else
+  {
+    printf("only 3/4 channel 1 byte per channel supported!\n");
+    return 0;
+  }
+	//type of compression 16 - 19
+  //pixel data size 20-24
+  /*
+  size_t pixel_data_size = 
+    (size_t)(file_data[ho + 20] | file_data[ho + 21] << 8 | file_data[ho + 22] << 16 | file_data[ho + 23] << 24);
+  size_t actual_data_width = pixel_data_size / img->height;
+
+  size_t data_width_in_bytes = img->width * (bits_per_pixel / 8);
+  size_t padding = actual_data_width - data_width_in_bytes; */
+
+
+  return 1;
 }
 
 
 img loadBmp(const char* path)
 {
-  img image;
-  image.width = 0;
-  image.height = 0;
-  image.channel_count = 0;
-  image.bytes_per_channel = 0;
-  image.pixel_data = NULL;
-  size_t fileSize;
-  char* img_file = loadFile(path, &fileSize);
-  if(!img_file) return image;
-  if(fileSize < BMP_FILE_HEADER_BYTES + BMP_INFO_HEADER_BYTES)
-    return image;
-  if(img_file[0] != 'B' || img_file[1] != 'M')
-    return image;
+  img image = emptyImageStruct();
 
-  size_t bmp_file_size = (size_t)(img_file[2] << 24 | img_file[3] << 16 | img_file[4] << 8 | img_file[5]);
-  printf("the size of the loaded bmp is %d bytes", bmp_file_size);
+  //load file 
+  size_t file_size;
+  char* file_data = loadFile(path, &file_size);
+  if(!file_data) 
+  {
+    free(file_data);
+    printf("failed to load file at %s\n", path);
+    return image;
+  }
 
-  free(img_file);
+  //check file valid
+
+  if(!checkValidBMPFile(file_size, file_data))
+  {
+    free(file_data);
+    printf("error at file: %s - invalid BMP file\n", path);
+    return image;
+  }
+
+  //read info header to fill out image struct info
+  if(!fillImageInfoFromFile(&image, file_data))
+  {
+    printf("error at file: %s - failed to fill image info from file info header\n", path);
+    return image;
+  }
+
+  //read pixel data into pixel array
+
+  free(file_data);
   return image;
 }
 
@@ -44,7 +133,7 @@ int saveBmp(img* image, const char* filename)
 {
   if(sizeof(char) != 1)
   {
-    printf("system not supported");
+    printf("system not supported, char isn't 1 byte\n");
     return IMG_FAILURE;
   }
 
@@ -94,15 +183,15 @@ int saveBmp(img* image, const char* filename)
 	info_header[6] = (char)(image->width >> 16);
 	info_header[7] = (char)(image->width >> 24);
 	//image height
-	info_header[8] = (char)image->height;
-	info_header[9] = (char)(image->height >> 8);
+	info_header[8]  = (char)image->height;
+	info_header[9]  = (char)(image->height >> 8);
 	info_header[10] = (char)(image->height >> 16);
 	info_header[11] = (char)(image->height >> 24);
 	//no. of planes (must be one)
 	info_header[12] = 0x01;
 	info_header[13] = 0x00;
 	//bits per pixel
-	info_header[14] = (char)(image->bytes_per_channel * image->channel_count * 8);
+	info_header[14] = (char) (image->bytes_per_channel * image->channel_count * 8);
 	info_header[15] = (char)((image->bytes_per_channel * image->channel_count * 8) >> 8);
 	//type of compression (0 - no compression)
 	info_header[16] = 0;
@@ -179,22 +268,8 @@ int saveBmp(img* image, const char* filename)
 
 void testBmpFileSaving()
 {
-  img test_image;
-  test_image.width = 1000;
-  test_image.height = 1000;
-  test_image.channel_count = 4;
-  test_image.bytes_per_channel = 1;
-  size_t img_size = test_image.width * test_image.height * test_image.channel_count * test_image.bytes_per_channel;
-  test_image.pixel_data = (char*)malloc(img_size);
-  for(size_t i = 0; i < img_size; i++)
-  {
-    test_image.pixel_data[i] = 0xFF;
-  }
-  if(saveBmp(&test_image, "test.bmp"))
-    printf("sucessfully saved test bmp\n");
-  else
-    printf("failed to save test bmp\n");
 
+  img test_image = loadBmp("test.bmp");
 
   free(test_image.pixel_data);
 }
--- src/img.h
+++ src/img.h
@@ -18,12 +18,23 @@ typedef struct s_img{
   char* pixel_data;
 } img;
 
+img emptyImageStruct()
+{
+  img image;
+  image.width = 0;
+  image.height = 0;
+  image.channel_count = 0;
+  image.bytes_per_channel = 0;
+  image.pixel_data = NULL;
+  return image;
+}
+
 
 void setPixel(img* image, uint x, uint y, char colour[4])
 {
   if(x > image->width || x < 0 || y > image->height || y < 0)
   {
-    printf("setPixel() params out of range error!");
+    printf("setPixel() params out of range error!\n");
     return;
   }
   for(size_t i = 0; i < image->channel_count; i++)
@@ -36,27 +47,45 @@ void setPixel(img* image, uint x, uint y, char colour[4])
 
 char* loadFile(const char* path, size_t* img_file_size)
 {
-  FILE* img_file = fopen(path, "rb"); //rb - r=read b=binary
+  //open file
+  FILE* img_file = fopen(path, "rb");
   if(!img_file)
   {
-    printf("failed to load file %s !", path);
+    printf("failed to load file %s !\n", path);
     return NULL;
   }
-  fseek(img_file, 0L, SEEK_END);
+
+  //get file size
+  if(fseek(img_file, 0L, SEEK_END) != 0)
+  {
+    printf("failed to find end of file at %s\n", path);
+    return NULL;
+  }
   *img_file_size = ftell(img_file);
+   if(fseek(img_file, 0L, SEEK_SET) != 0)
+  {
+    printf("failed to find start of file at %s\n", path);
+    return NULL;
+  }
+
+  //create buffer for file data
   char *img_file_data = (char*)malloc(*img_file_size);
   if(!img_file_data)
   {
     fclose(img_file);
-    printf("failed to allocate memory for %s !", path);
+    printf("failed to allocate memory for %s !\n", path);
     return NULL;
   }
-  if( 1!=fread(img_file_data , *img_file_size, 1 , img_file) )
+
+  //read file data into buffer
+  if(fread(img_file_data , *img_file_size, 1 , img_file) != 1)
   {
-        fclose(img_file);
-        free(img_file_data);
-        return NULL;
+    printf("failed to load image at %s into buffer\n", path);
+    fclose(img_file);
+    free(img_file_data);
+    return NULL;
   }
+
   fclose(img_file);
   return img_file_data;
 }
